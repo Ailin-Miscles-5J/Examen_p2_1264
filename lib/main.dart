d7432cc0887c50382ed6351d5736941a3358d309
CITLALI_AILEEN_MISCLES_RUVALCABA <a.22308051281264@cbtis128.edu.mx>
2025-03-12 19:13:08 +0000

    subir de nuevo

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,108 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
+
+class Proveedor {
+  final String idProveedor;
+  final String telefono;
+  final String correo;
+
+  Proveedor({
+    required this.idProveedor,
+    required this.telefono,
+    required this.correo,
+  });
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  final List<Proveedor> proveedores = [
+    Proveedor(
+        idProveedor: '001',
+        telefono: '555-1234',
+        correo: 'proveedor1@example.com'),
+    Proveedor(
+        idProveedor: '002',
+        telefono: '555-5678',
+        correo: 'proveedor2@example.com'),
+    Proveedor(
+        idProveedor: '003',
+        telefono: '555-9012',
+        correo: 'proveedor3@example.com'),
+    // Agregar dos proveedores más
+    Proveedor(
+        idProveedor: '004',
+        telefono: '555-3456',
+        correo: 'proveedor4@example.com'),
+    Proveedor(
+        idProveedor: '005',
+        telefono: '555-7890',
+        correo: 'proveedor5@example.com'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Lista de Proveedores'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: proveedores.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Colors.blue[50],
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'ID Proveedor: ${proveedores[index].idProveedor}',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        Divider(color: Colors.blue),
+                        Text(
+                          'Teléfono: ${proveedores[index].telefono}',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                        Divider(color: Colors.blue),
+                        Text(
+                          'Correo: ${proveedores[index].correo}',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Citlali Miscles 1264',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
         ),
       ),
     );
